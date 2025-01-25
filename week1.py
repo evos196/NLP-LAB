239cbd56c2c3a03ba17f8d335f6bc095acac2571
evos456 <161448262+evos456@users.noreply.github.com>
2025-01-25 14:56:16 +0530

    Update week1.py

--- week1.py
+++ week1.py
@@ -10,16 +10,16 @@ nltk.download('punkt_tab')
 
 def word_analysis(text):
   words=word_tokenize(text)
-  word=[word.lower() for word in words]
+  words=[word.lower()for word in words]
   stop_words=set(stopwords.words('english'))
-  words=[word for word in words if word.isalnum and word not in stop_words]
+  words=[word for word in words if word.isalnum() and word not in stop_words]
   freq_dist=FreqDist(words)
-
-  print("total words:",len(words))
+  
+  print("Total words:",len(words))
   print("Unique words:",len(freq_dist))
   print("Most common words:")
   print(freq_dist.most_common(10))
 
-if __name__ == "__main__":
+if __name__=="__main__":
   text="Natural language processing (NLP) is a subfield of computer science and artificial intelligence (AI) that uses machine learning to enable computers to understand and communicate with human language."
   word_analysis(text)
